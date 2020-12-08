1227c982ef32f766896060c6bb0b6d96683c8950
aortiz34 <aortiz34@ucol.mx>
2020-12-07 18:54:25 -0600

    Modified the second function

--- src/main.js
+++ src/main.js
@@ -12,10 +12,10 @@ export default class App {
           let suma = 0;
           let i = 1;
           while (i <= numero) {
-              if (i % 2 === 0){
-                  suma += 1 / i;
+              if (i % 2 !== 0 && i !== 1){
+                  suma -= (1 / i);
               }else {
-                  suma -= 1 / i;
+                  suma += (1 / i);
               }
               i += 1;
           }
@@ -31,4 +31,4 @@ console.log(app.sumatoriaSerieUno(6));
 console.log(app.sumatoriaSerieUno(8));
 
 console.log(app.sumatoriaSerieDos(6));
-console.log(app.sumatoriaSerieDos(6));
+console.log(app.sumatoriaSerieDos(1));
