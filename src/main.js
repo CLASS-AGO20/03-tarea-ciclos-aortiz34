71e704fdb557bfb467f7ce8507cd544b50bff554
aortiz34 <aortiz34@ucol.mx>
2020-12-07 18:19:28 -0600

    Added the first function

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-export default class App {}
+export default class App {
 
+    sumatoriaSerieUno(numero) {
+        let suma = 0;
+        for (let i  = 1; i <= numero; i ++) {
+            suma += 1 / i;
+        }
+        return suma;
+      }
+
+
+
+}
 let app = new App();
+
+console.log(app.sumatoriaSerieUno(6));
+console.log(app.sumatoriaSerieUno(8));
